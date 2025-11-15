6e6037eabc6c179ce150e453b067882ac00b1ab9
Albaraa Alkilani <baraa.alkilani@gmail.com>
2025-11-15 15:52:33 +0300

    Add files via upload

---
+++ loadQubeParametersSandB.m
@@ -0,0 +1,81 @@
+% LOADQUBEPARAMETERS loads parameters for Qube Swing Up Demo
+
+% Copyright 2021 The MathWorks, Inc.
+
+%% Plant parameters
+R_m = 21.7;         % Motor resistance [Ohm]
+mu_m = 3.08e-6;     % Damping of motor shaft [Nm/rad/s]
+% K_t = 0.042;        % Torque constant [Nm/A]
+K_b = 0.0392;       % Back EMF contant [V/rad/s]
+K_b_2 = 0.182;      % Back EMF contant [V/rad/s]
+J_m = 4e-6;         % Motor shaft inertia [kg*m^2]
+L_m = 4.98e-3;      % Motor Inductance [H]
+rod_rad = 0.003;    % Arm rod radius [m]
+p_rad = 0.0045;     % Pendulum radius [m]
+L_p = 0.126;        % Length of Pendulum [m]
+L_r = 0.103;        % Length of Arm rod [m]
+m_p = 0.024;        % Mass of Pendulum [kg]
+m_r = 0.095;        % Mass of Arm rod [kg]
+J_p = m_p*(p_rad^2/4+L_p^2/12) + m_p*(L_p/2)^2;     % Inertia of Pendulum [kg*m^2]
+J_r = m_r*(rod_rad^2/4+L_r^2/12) + m_r*(L_r/2)^2;   % Inertia of Arm rod [kg*m^2]
+D_r = 0.001;        % Damping of Arm rod [Nm/rad/s]
+D_r_2 = 1.88e-04;   % Damping of Arm rod [Nm/rad/s]
+D_p = 8e-6;         % Damping og Pendulum [Nm/rad/s]
+n = 1;              % Gera ratio
+g = 9.81;           % Gravity [m/s^2]
+Volt_Limit = 12;
+angle_limit = 3*pi/4;
+
+%% Sample time
+Tf = 20;               % Simulation stop time. [s]
+Ts = 0.01;           % Time step for Reinforcement Learning agent. [s]
+
+
+%% Initial condition
+theta0 = 0;
+phi0 = 0;
+dtheta0 = 0;
+dphi0 = 0;
+
+%% PID gains
+%theta_gain   = 0.162;
+%dtheta_gain  = 0.0356;
+%phi_gain     = 40;
+%dphi_gain    = 2;
+
+%% Configuration
+load("configs.mat")
+
+%% reward fun parameters
+q11=0.166;
+q22=0.5;
+q33=0.03;
+q44=0.01;
+r=0.001;
+B=-100;
+succeed=100;
+theta_max=3*pi/4;
+alpha_bal_threshold=pi/6;
+swing_threshold=0.01;
+Vmax=12;
+thetaAccepted=0;
+phiAccepted=0;
+
+%% define enviroment 
+
+%Tf=10
+%Ts=.001
+rng(0)
+obsInfo=rlNumericSpec([4 1],'LowerLimit',[-inf,-inf,-inf,-inf]','UpperLimit',[inf,inf,inf,inf]');
+obsInfo.Name='observation';
+obsInfo.Description='theta,alpha,theta_dot,alpha_dot';
+numObs=obsInfo.Dimension(1);
+actInfo=rlNumericSpec([1 1],'LowerLimit',-Vmax,'UpperLimit',Vmax);
+actInfo.Name=['Motor Voltage'];
+mdl='QubeServo2_Inverted_Pendulum_System';
+open_system(mdl)
+%SandBAgent1 = mdl + "/Subsystem1/RL Agent2";
+%SandBAgent2 = mdl + "/Subsystem/RL Agent3";
+
+%simEnv_SB = rlSimulinkEnv(mdl,[SandBAgent1,SandBAgent2],obsInfo,actInfo);
+%simEnv_3.ResetFcn = @localResettFcnSandB;
